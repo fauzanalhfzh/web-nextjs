baf0ccfcfbd0c0d0f01c6d57c08884cbd33315ed
fauzanalhfzh <zenbangga@gmail.com>
2024-12-16 18:57:50 +0700

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+        <h1 className='text-2xl font-bold'>About Page</h1>
+        <a href="http://localhost:3000" className="text-blue-500 underline">Home Page</a>
+    </div>
+  )
+}
+
+export default About
